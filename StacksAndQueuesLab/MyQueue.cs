c0e16f683b5a2341a4028e1d3235e88370dec7e4
SparkSages <brendan.dalhover@gmail.com>
2023-10-05 13:57:17 -0400

    Initial commit

---
+++ StacksAndQueuesLab/MyQueue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueuesLab
+{
+    public class MyQueue<T> : IQueue<T>
+    {
+        private readonly List<T> elements;
+        public int Count => elements.Count;
+        public MyQueue()
+        {
+            elements = new List<T>();
+        }
+        public T Dequeue()
+        {
+            if (elements.Count == 0)
+                throw new InvalidOperationException("Queue is empty!");
+            T element = elements[0];
+            elements.RemoveAt(0);
+            return element;
+        }
+        public void Enqueue(T item)
+        {
+            elements.Add(item);
+        }
+        public T Peek()
+        {
+            return elements[0];
+        }
+    }
+}
